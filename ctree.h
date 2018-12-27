8c222a7318d47a8837999fabb0f54524cbfb8427
Qu Wenruo <wqu@suse.com>
2018-12-27 10:06:51 +0800

    btrfs-progs: Refactor btrfs_read_block_groups()
    This patch does the following refactor:
    - Refactor parameter from @root to @fs_info
    
    - Refactor the large loop body into another function
      Now we have a helper function, read_one_block_group(), to handle
      block group cache and space info related routine.
    
    - Refactor the return value
      Even we have the code handling ret > 0 from find_first_block_group(),
      it never works, as when there is no more block group,
      find_first_block_group() just return -ENOENT other than 1.
    
      This is super confusing, it's almost a mircle it even works.
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>

--- ctree.h
+++ ctree.h
@@ -2539,7 +2539,7 @@ int btrfs_update_extent_ref(struct btrfs_trans_handle *trans,
 			    u64 owner_objectid);
 int btrfs_write_dirty_block_groups(struct btrfs_trans_handle *trans);
 int btrfs_free_block_groups(struct btrfs_fs_info *info);
-int btrfs_read_block_groups(struct btrfs_root *root);
+int btrfs_read_block_groups(struct btrfs_fs_info *info);
 struct btrfs_block_group_cache *
 btrfs_add_block_group(struct btrfs_fs_info *fs_info, u64 bytes_used, u64 type,
 		      u64 chunk_offset, u64 size);
